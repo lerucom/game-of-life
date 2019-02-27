4fcad89b1aca2f5b602511dc418dce01231fe9ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-27 20:16:54 +0000

    Deploy lerucom/game-of-life to github.com/lerucom/game-of-life.git:gh-pages

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const GRID_WIDTH = 1280;
 const GRID_HEIGHT = 720;
 const GRID_ROWS = 36;
 const GRID_COLS = 64;
-const GAME_SPEED = 100;
+const GAME_SPEED = 150;
 
 const grid = createGrid(GRID_ROWS, GRID_COLS);
 const nextGrid = createGrid(GRID_ROWS, GRID_COLS);
@@ -12,6 +12,11 @@ let isPlaying = false;
 const root = document.querySelector('#root');
 const table = createTable(GRID_ROWS, GRID_COLS);
 createControls();
+
+function play() {
+    computeNextGrid();
+    updateView();
+}
 
 function updateView() {
     for (let i = 0; i < grid.length; i++) {
@@ -65,6 +70,8 @@ function createTable(rows, cols) {
     return table;
 }
 
+// const interval = setInterval(play, GAME_SPEED);
+
 function createControls() {
     const startButton = document.createElement('button');
     startButton.className = 'material-icons';
@@ -73,9 +80,11 @@ function createControls() {
         if (isPlaying) {
             isPlaying = false;
             evt.target.textContent = 'play_arrow';
+            clearInterval(interval);
         } else {
             isPlaying = true;
             evt.target.textContent = 'pause';
+            interval = setInterval(play, GAME_SPEED);
         }
     });
 
@@ -85,6 +94,10 @@ function createControls() {
     resetButton.addEventListener('click', (evt) => {
         isPlaying = false;
         startButton.textContent = 'play_arrow';
+
+        resetGrid();
+        updateView();
+        clearInterval(interval);
     });
 
     const randomizeButton = document.createElement('button');
@@ -93,10 +106,10 @@ function createControls() {
     randomizeButton.addEventListener('click', (evt) => {
         isPlaying = false;
         startButton.textContent = 'play_arrow';
-        
+
         randomizeGrid();
-        console.log(grid);
         updateView();
+        clearInterval(interval);
     });
 
 
@@ -128,4 +141,108 @@ function randomizeGrid() {
             grid[i][j] = Math.round(Math.random());
         }
     }
+}
+
+function resetGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+function computeNextGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            applyRules(i, j);
+        }
+    }
+
+    copyNextGrid();
+}
+
+function copyNextGrid() {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            grid[i][j] = nextGrid[i][j];
+            nextGrid[i][j] = 0;
+        }
+    }
+}
+
+function applyRules(row, col) {
+    const isCellAlive = grid[row][col];
+    const numberOfNeighbors = countNeighbors(row, col);
+
+    if (isCellAlive) {
+        if (numberOfNeighbors < 2) {
+            // cell dies
+            nextGrid[row][col] = 0;
+        } else if (numberOfNeighbors === 2 || numberOfNeighbors === 3) {
+            // cell lives
+            nextGrid[row][col] = 1;
+        } else if (numberOfNeighbors > 3) {
+            // cell dies
+            nextGrid[row][col] = 0;
+        } 
+    } else {
+        if (numberOfNeighbors === 3) {
+            // cell lives
+            nextGrid[row][col] = 1;
+        }
+    }
+}
+
+function countNeighbors(row, col) {
+    let count = 0;
+
+    if (row - 1 >= 0) { // top
+        if (grid[row - 1][col] === 1) {
+            count++;
+        }
+    }
+    
+    if ((row - 1 >= 0) && (col - 1 >= 0)) { // top left
+        if (grid[row - 1][col - 1] === 1) {
+            count++;
+        }
+    }
+
+    if ((row - 1 >= 0) && (col + 1 < GRID_COLS)) { // top right
+        if (grid[row - 1][col + 1] === 1) {
+            count++;
+        }
+    }
+
+    if (col - 1 >= 0) { // left
+        if (grid[row][col - 1] === 1) {
+            count++;
+        }
+    }
+
+    if (col + 1 < GRID_COLS) { // right
+        if (grid[row][col + 1] === 1) {
+            count++;
+        }
+    }
+
+    if (row + 1 < GRID_ROWS) { // bottom
+        if (grid[row + 1][col] === 1) {
+            count++;
+        }
+    }
+
+    if ((row + 1 < GRID_ROWS) && (col - 1 >= 0)) { // bottom left
+        if (grid[row + 1][col - 1] === 1) {
+            count++
+        }
+    }
+
+    if ((row + 1 < GRID_ROWS) && (col + 1 < GRID_COLS)) { // bottom right
+        if (grid[row + 1][col + 1] === 1) {
+            count++;
+        }
+    }
+
+    return count;
 }
